e53f079422a29aa3b1f1bfda49fc13489f64556f
Bence Sziver <sziver98@gmail.com>
2023-07-07 12:46:09 +0200

    input system setup & wheel collider setup & rigidbody setup

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+// Using embedded actions with callbacks or reading values each frame.
 
 public class CarController : MonoBehaviour
 {
+    private PlayerInput playerInput;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +22,15 @@ public class CarController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        Vector2 moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
+
+        float acceleration = moveInput.y;
+        float steering = moveInput.x;
+        Move(acceleration, steering, 0);
+    }
+
+    private void Move(float acceleration, float steering, float braking)
     {
         
     }
