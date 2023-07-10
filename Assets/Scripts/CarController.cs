e319e57ae2c85e7f5fcd0e2826e75bdbb86beaa7
Bence Sziver <sziver98@gmail.com>
2023-07-10 18:15:18 +0200

    sphereController method prototype

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,27 +1,23 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-// Using embedded actions with callbacks or reading values each frame.
-
 public class CarController : MonoBehaviour
 {
-    // the collection of wheels
-    [SerializeField] private WheelCollider[] _wheelColliders;
-    // torque
-    [SerializeField] private float torque = 1200.0f;
-    [SerializeField] private float _maxSteeringAngle = 30.0f;
-    // maximum braking torque
-    [SerializeField] private float _maxBrakingTorque = 550.0f;
-    [SerializeField] private AudioClip skidSoundEffect;
-    [SerializeField] private float _skidThreshold = 0.4f;
+    public Rigidbody sphereRb;
     private PlayerInput playerInput;
-    private AudioSource _audioSource;
-    private float braking;
+    private float acceleration;
+    public float speed;
+    public float maxSteerAngle;
 
     private void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
-        _audioSource = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
     }
 
     // Update is called once per frame
@@ -29,78 +25,15 @@ public class CarController : MonoBehaviour
     {
         Vector2 moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
         float acceleration = moveInput.y;
-        float steering = moveInput.x;
-        Move(acceleration, steering, braking);
-    }
-
-    private void Move(float acceleration, float steering, float braking)
-    {
-        // ensures the values are clamped
+        // float steering = moveInput.x;
         acceleration = Mathf.Clamp(acceleration, -1f, 1f);
-        steering = Mathf.Clamp(steering, -1f, 1f) * _maxSteeringAngle;
-        braking = Mathf.Clamp(braking, -1f, 1f) * _maxBrakingTorque;
-        // calculate the thrust torque
-        float thrustTorque = acceleration * torque;
+        // steering = Mathf.Clamp(steering, -1f, 1f) * maxSteerAngle;
 
-        // apply thrust torque to each wheel
-        for (int i = 0; i < _wheelColliders.Length; i++)
-        {
-            _wheelColliders[i].motorTorque = thrustTorque;
-            // get the position and rotation of the wheel collider
-            _wheelColliders[i].GetWorldPose(out Vector3 position, out Quaternion quaternion);
-            // reposition the game object with the mesh of the wheel
-            // apply the rotation to the game object
-            // wheel.transform.GetChild(0).transform.SetPositionAndRotation(position, quaternion);
-            _wheelColliders[i].transform.GetChild(0).Rotate(-_wheelColliders[i].rpm / 60 * 360 * Time.deltaTime, 0, 0);
-            
-            // apply steering to the front wheels and braking to the rear
-            if (i < 2)
-            {
-                _wheelColliders[i].steerAngle = steering;
-            }   else
-            {
-                _wheelColliders[i].brakeTorque = braking;
-            }
-        }
-        SkidCheck();
+        transform.position = sphereRb.transform.position;
     }
 
-    private void SkidCheck()
+    private void FixedUpdate()
     {
-        int skidCount = 0;
-        for (int i = 0; i < _wheelColliders.Length; i++)
-        {
-            // get the point on the ground where the wheel collider is touching
-            WheelHit wheelHit;
-            _wheelColliders[i].GetGroundHit(out wheelHit);
-
-            // check if there's any forward or sideway slipping
-            if (Mathf.Abs(wheelHit.forwardSlip) >= _skidThreshold || Mathf.Abs(wheelHit.sidewaysSlip) >= _skidThreshold) {
-                skidCount++;
-                // check the sound isn't playing
-                if (!_audioSource.isPlaying)
-                {
-                    // play the skidding sound effect
-                    _audioSource.PlayOneShot(skidSoundEffect);
-                }
-            }
-        }
-
-        // turn off the skidding sound if there is no wheels skidding
-        if (skidCount == 0 && _audioSource.isPlaying)
-        {
-            _audioSource.Stop();
-        }
-    }
-
-    public void Braking(InputAction.CallbackContext context)
-    {
-        if (context.started)
-        {
-            braking = 1.0f;
-        }   else if (context.canceled)
-        {
-            braking = 0.0f;
-        }
+        sphereRb.AddForce(acceleration * transform.forward, ForceMode.Acceleration);
     }
 }
